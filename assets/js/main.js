4abae2d85c85a77e5a716f0f1d74666838541c4a
Rahel Weiss <mail@rahelweiss.com>
2018-09-21 15:52:19 +0200

    installed swiper except in html

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,3 +93,26 @@ function stickyNav() {
     arrow.classList.remove("clear");
   }
 }
+
+// Swiper
+var mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination"
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar"
+  }
+});
--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,4 +1,5 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="path/to/swiper.min.js"></script>
     <script type="text/javascript" src="assets/build/script.min.js"></script>
 
   </body>
